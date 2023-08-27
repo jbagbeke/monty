9325843e28345117e2e3ea0df990ee945f1b0844
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-27 22:27:07 +0000

    Stacks

--- monty_exe.c
+++ monty_exe.c
@@ -14,19 +14,29 @@ int monty_exe(char **b, int func_num, unsigned int num)
 {
 	int count, i;
 	instruction_t *inst;
-	stack_t *monty_stack;
+	stack_t *monty_stack, *tmp_stack;
 
 	count = count_tokens(b);
-
-	if (count == 1 && strcmp(b[0], "push") == 0)
+	
+	if (count == 1)
+	{
+	if (strcmp(b[0], "push") == 0)
 		push_error(num);
 
+	inst = select_funct(func_num, b[0]);
+
+	if (strcmp(b[0], "push") != 0)
+		inst->f(&tmp_stack, num);
+	}
+
+	if (count > 1)
+	{
 	for (i = 1; i < count; i++)
 	{
 		if (check(b[i]) == 0)
 	{
 		inst = select_funct(func_num, b[0]);
-	
+
 		if (strcmp(b[0], "push") == 0)
 		{
 			monty_stack = malloc(sizeof(stack_t));
@@ -38,10 +48,11 @@ int monty_exe(char **b, int func_num, unsigned int num)
 			}
 
 			monty_stack->n = atoi(b[i]);
-		}
 			inst->f(&monty_stack, num);
+		}
 	}
 
+	}
 	}
 	return (0);
 }
@@ -85,21 +96,19 @@ void monty_push(stack_t **stack, unsigned int line_number)
 */
 void monty_pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *ptr, *ptr2;
+	stack_t *ptr2;
 	int num, i;
 
 	(void)line_number;
-	(void)stack;
-
-	ptr = monty_head;
 	ptr2 = monty_head;
-	while (ptr->next != NULL)
-		ptr = ptr->next;
+	*stack = monty_head;
+	while ((*stack)->next != NULL)
+		*stack = (*stack)->next;
 
 	num = monty_calc(ptr2);
 	for (i = 0; i < num; i++)
 	{
-		printf("%d\n", ptr->n);
-		ptr = ptr->prev;
+		printf("%d\n", (*stack)->n);
+		*stack = (*stack)->prev;
 	}
 }
